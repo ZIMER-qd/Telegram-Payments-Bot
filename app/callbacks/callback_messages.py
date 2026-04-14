1d3febd9354331348f0db07cc15f7c06b6485d98
Vadim Filippov <vadim05filippov@gmail.com>
2026-04-14 20:41:44 +0300

    Add provider token and start database setup

---
+++ app/callbacks/callback_messages.py
@@ -0,0 +1,5 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+
+router = Router()
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    provider_token: SecretStr
     model_config: SettingsConfigDict = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8'
